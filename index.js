4e00a6f48598ea510c063d769997ada692a3b8a1
sihanas <iamsihanas@gmail.com>
2025-06-26 21:57:44 +0530

    modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ let isAlive = true
 
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
 }
-
-console.log(message)
